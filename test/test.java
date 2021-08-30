e4997433f36a90b147ed009d47ac8d7579a74e6f
ti777 <tifany090295@hotmail.com>
2021-08-30 20:53:43 +0200

    Initial commit

---
+++ test/test.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class test {
+
+    @Test
+    void shouldConvert1ToI() {
+        assertEquals("I", toRoman(1));
+    }
+
+    private String toRoman(int value) {
+        return "";
+    }
+}
